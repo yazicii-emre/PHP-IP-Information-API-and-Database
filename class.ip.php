7d8143cf0efd4bcf9e89d4b45f54e34a0f874740
Emre Yazıcı <67283214+nevermind-the-name@users.noreply.github.com>
2021-08-27 14:05:03 +0300

    Update class.ip.php

--- class.ip.php
+++ class.ip.php
@@ -1,31 +1,34 @@
-<?php 
+<?php
 
+// İp Sınıfımızı Dahil Ediyoruz.
 require_once 'class.db.php';
 
 
 class ip extends db {
-	
 
-public $ip;
 
+// Oluşturulacak Tablo Adını Buraya Yazıyoruz.
 private $tableName='ipapi';
-	
-	
+
+
+
+// Bu Metod , Eğer İp Api Bağlantısı Başarılı İse İp Bilgilerinin Olduğu Bir Dizi Değer Döndürecek
 public function ipConnection(){
-	
+
 	try {
-		
+
 	$query = unserialize(file_get_contents("http://ip-api.com/php/{$this->get_client_ip()}?fields=124411"));
- 
-	if ($query['status']!='success') { 
-	throw new PDOException ('Api bağlantısı başarısız ...');		
+
+	if ($query['status']!='success') {
+	throw new PDOException ('Api bağlantısı başarısız ...');
 	}
-		
+
+
+   // $this->tableName'de Belirttiğilen Tablo Adında Bir Tablomuz Yok İse Tablo Oluşturuyor
 	 $table_exists =$this->db->prepare( "DESCRIBE $this->tableName ");
-		
+
 if ( !$table_exists->execute() ) {
-    // my_table exists
-	
+
 	$createTable= "CREATE TABLE $this->tableName (
 	ip_id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
 	insert_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -41,74 +44,76 @@ if ( !$table_exists->execute() ) {
 	org VARCHAR(75) NOT NULL,
 	mobile ENUM('0','1') DEFAULT '0',
 	query VARCHAR(75) NOT NULL
-	
-	)	
-	";
-	
 
-	$isCreate=$this->db->exec($createTable);		
-	
-} 
-		
-			
-	return $query;	
-		
+	)
+	";
+
+	$isCreate=$this->db->exec($createTable);
+
+}
+
+
+	return $query;
+
 	}catch (PDOException $e) {
-		
+
 		return 'Bir hata oluştu! '.$e->getMessage();
 	}
-	
-	
-}	
-	
-	
-public function timeDifference($firstTime,$secondTime) {
-	
- date_default_timezone_get("Europe/İstanbul");
-	
- $first=strtotime($firstTime);	
- $second=strtotime($secondTime);
-	
- $totalTime=($second-$first)/86400;
-	
- return round($totalTime);	
-	
-}	
-	
+
+
+}
+
+
+// Bu Metodu Çağırarak İp Kayıt/Güncelleme İşlemlerini Gerçekleştiriyoruz
 public function setIp() {
-	
-	
+
 	try {
-		
+
 	if (is_array($this->ipConnection())) {
 
-		
 		$select=$this->db->prepare("SELECT * FROM $this->tableName WHERE query=?");
 		$select->execute([ $this->get_client_ip() ]);
-		
-		if ($select->rowCount()==0) {
-			
-        $insert=$this->db->prepare("INSERT INTO $this->tableName SET {$this->setValue($this->ipConnection())}");
-		$insert->execute( $this->addValue($this->ipConnection()) ); 
-			
-			
+
+
+		if (!$select->rowCount()) {
+
+			// İp Adresi Kayıtlı Değilse Kayıt İşlemi Gerçekleşiyor
+        $stmt=$this->db->prepare("INSERT INTO $this->tableName SET
+        status=? ,country=?, countryCode=?, regionName=?,city=?, lat=?,lon=?,timezone=?,org=?,mobile=?,query=?
+        ");
+
+		}else {
+
+// İp Adresi Kayıtlı İse Ve Kayıt Üzerinden 1 Hafta Geçmiş İse Güncelleme İşlemi Gerçekleşiyor
+			$stmt=$this->db->prepare("UPDATE $this->tableName SET
+			insert_time={date('Y-m-d H:i:s')},status=? ,country=?, countryCode=?, regionName=?,city=?, lat=?,lon=?,timezone=?,org=?,mobile=? where insert_time < DATE_SUB(Now(),INTERVAL 1 MONTH) and  query=?
+			");
+
 		}
-			
-		
-	
+
+
+		$stmt->execute( [
+  $this->ipConnection()['status'],$this->ipConnection()['country'],$this->ipConnection()['countryCode'],$this->ipConnection()['regionName'],$this->ipConnection()['city'],
+	$this->ipConnection()['lat'],$this->ipConnection()['lon'],$this->ipConnection()['timezone'],$this->ipConnection()['org'],$this->ipConnection()['mobile'],
+	$this->ipConnection()['query']
+
+			] );
+
+
 	}else {
 		echo $this->ipConnection();
-	}	
+	}
 
-		
-	
-	}catch (PDOException $e) {		
+
+	}catch (PDOException $e) {
 		return $e->getMessage();
 	}
-	
-}	
-	
-	
+
+}
+
+
+
+// İp Adresini Döndürüyor.
  private function get_client_ip() {
     $ipaddress = '';
     if ( getenv( 'HTTP_CLIENT_IP' ) )
@@ -126,10 +131,10 @@ public function setIp() {
     else
       $ipaddress = 'UNKNOWN';
     return $ipaddress;
-  }	
-	
-	
-	
+  }
+
+
+
 }
 
 ?>
